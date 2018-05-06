b8f3d3890aad255719a930afc7017ce68e162939
Kanishk Varshney <varskann1993@gmail.com>
2018-05-07 00:01:59 +0530

    Blog Added

--- HomePage/urls.py
+++ HomePage/urls.py
@@ -4,6 +4,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('about/', views.AboutPageView.as_view(), name='about'),
-    url('', views.HomePageView.as_view(), name='home'),
+    url(r'^blog/(?P<pk>\d+)/$', views.BlogDetailView.as_view(), name='blog_detail'),
+    url(r'^blog/$', views.BlogListView.as_view(), name='blog'),
+    url(r'^about/$', views.AboutPageView.as_view(), name='about'),
+    url(r'^$', views.HomePageView.as_view(), name='home'),
+
 ]
--- HomePage/views.py
+++ HomePage/views.py
@@ -2,8 +2,10 @@
 from __future__ import unicode_literals
 
 # pages/views.py
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from . models import Post
 
+from django.shortcuts import render, get_object_or_404
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
@@ -11,3 +13,35 @@ class HomePageView(TemplateView):
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blog.html'
+    context_object_name = 'blogs'
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blog_detail.html'
+    context_object_name = 'blog'
+
+
+# def post_detail(request, id):
+#     template = 'blog_detail.html'
+#     print "Here"
+#     blog = get_object_or_404(Post, id=id)
+#     context = {
+#         'blog': blog,
+#     }
+#     print blog
+#     return render(request, template, context)
+#
+#
+# def post_list(request):
+#     template = 'blog.html'
+#     objects_list = Post.objects.all()
+#     print "Here WTF"
+#     context = {
+#         'blogs': objects_list,
+#     }
+#     return render(request, template, context)
